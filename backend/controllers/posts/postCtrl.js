551f9af7ffea7922c0f9ddf0a0f746654282be4f
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2022-07-03 18:29:06 -0500

    Dislike Posts and populate dislikes and likes with users
    Add dislikes to the post and dislikes.
    Adding population of users when you fetched a single post.

--- backend/controllers/posts/postCtrl.js
+++ backend/controllers/posts/postCtrl.js
@@ -68,7 +68,10 @@ const fetchPostCtrl = expressAsyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongodbId(id);
   try {
-    const post = await Post.findById(id).populate("user");
+    const post = await Post.findById(id)
+      .populate("user")
+      .populate("disLikes")
+      .populate("likes");
     // UPDATE NUMBER OF VIEWS
     await Post.findByIdAndUpdate(
       id,
@@ -172,7 +175,57 @@ const toggleAddLikeToPostCtrl = expressAsyncHandler(async (req, res) => {
   }
 });
 
+const toggleAddDislikeToPostCtrl = expressAsyncHandler(async (req, res) => {
+  //1.Find the post to be disLiked
+  const { postId } = req.body;
+  const post = await Post.findById(postId);
+  //2.Find the login user
+  const loginUserId = req?.user?._id;
+  //3.Check if this user has already disLikes
+  const isDisLiked = post?.isDisLiked;
+  //4. Check if already like this post
+  const alreadyLiked = post?.likes?.find(
+    (userId) => userId.toString() === loginUserId?.toString()
+  );
+  //Remove this user from likes array if it exists
+  if (alreadyLiked) {
+    const post = await Post.findOneAndUpdate(
+      postId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    );
+    res.json(post);
+  }
+  //Toggling
+  //Remove this user from dislikes if already disliked
+  if (isDisLiked) {
+    const post = await Post.findByIdAndUpdate(
+      postId,
+      {
+        $pull: { disLikes: loginUserId },
+        isDisLiked: false,
+      },
+      { new: true }
+    );
+    res.json(post);
+  } else {
+    const post = await Post.findByIdAndUpdate(
+      postId,
+      {
+        $push: { disLikes: loginUserId },
+        isDisLiked: true,
+      },
+      { new: true }
+    );
+    res.json(post);
+  }
+});
+
 module.exports = {
+  toggleAddDislikeToPostCtrl,
   toggleAddLikeToPostCtrl,
   deletePostCtrl,
   updatePostCtrl,
